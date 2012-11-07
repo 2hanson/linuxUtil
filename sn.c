c235cc45b9a035f471c53e303bac49633ac8e42c
hanson <hangzhong.yu@gmail.com>
2012-11-07 11:02:12 +0800

    add sn.c: printf disk serial number

---
+++ sn.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <linux/hdreg.h>
+#include <sys/fcntl.h>
+
+int main()
+{
+	struct hd_driveid id;
+
+	int fd = open("/dev/sda11", O_RDONLY|O_NONBLOCK);
+
+	if (fd < 0)
+	{
+		perror("/dev/hda");
+		return 1;
+	}
+	if (!ioctl(fd, HDIO_GET_IDENTITY, &id))
+	{
+		printf("serial number = %s\n", id.serial_no);
+	}
+
+	return 0;
+}
